8b33d3fe69373e2bf886374205714a4c7f23baac
leo-me <leo.me0602@gmail.com>
2022-07-20 00:32:22 +0800

    720

---
+++ 15.三数之和.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: leo-me leo.me0602@gmail.com
+ * @Date: 2022-07-18 23:34:48
+ * @LastEditors: leo-me leo.me0602@gmail.com
+ * @LastEditTime: 2022-07-19 00:05:50
+ * @FilePath: /.leetcode/15.三数之和.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+/*
+ * @lc app=leetcode.cn id=15 lang=javascript
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+
+    var nSum = function(nums, n, start, target) {
+        let sz = nums.length;
+        let res = [];
+        if(n<2 || sz < n) return res;
+
+        if(n === 2) {
+
+        } else {
+            // n > 2
+        }
+    }
+
+};
+// @lc code=end
+
