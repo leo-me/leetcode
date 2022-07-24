d7d40c123dda0eb50c682557c6e0126fc2bb19d1
Leo Lee <leo@Leos-MacBook-Pro.local>
2022-07-24 23:25:52 +0800

    7.24

--- 92.反转链表-ii.js
+++ 92.反转链表-ii.js
@@ -19,12 +19,13 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, left, right) {
-
-
+    // 后驱节点
     let successor = null;
 
+    // 反转以head 为起点的n个节点，返回新的头节点
     function reverseN(head, n) {
         if(n === 1) {
+            // 记录第n+1 个节点
             successor = head.next;
             return head;
         }
